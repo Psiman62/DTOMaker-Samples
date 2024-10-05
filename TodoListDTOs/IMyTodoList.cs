8a0cc6500f14f7f7a9bbcb17adf5f133781767f3
Psiman62 <simon_dudley@msn.com>
2024-10-05 21:42:25 +1000

    Core updates

--- TodoListDTOs/IMyTodoList.cs
+++ TodoListDTOs/IMyTodoList.cs
@@ -9,17 +9,17 @@ namespace TodoListDTOs
     [EntityLayout(LayoutMethod.Explicit, 32)]
     public interface IAllTypesExplicit
     {
-        [Member(1)][MemberLayout(0,1)] bool Field01 { get; }
-        [Member(2)][MemberLayout(1, 1)] sbyte Field02 { get; }
-        [Member(3)][MemberLayout(2, 1)] byte Field03 { get; }
-        [Member(4)][MemberLayout(4, 2)] short Field04 { get; }
-        [Member(5)][MemberLayout(6, 2)] ushort Field05 { get; }
-        [Member(6)][MemberLayout(8, 2)] char Field06 { get; }
-        [Member(7)][MemberLayout(12, 4)] int Field07 { get; }
-        [Member(8)][MemberLayout(16, 4)] uint Field08 { get; }
-        [Member(9)][MemberLayout(20, 4)] float Field09 { get; }
+        [Member(1)][MemberLayout(0)] bool Field01 { get; }
+        [Member(2)][MemberLayout(1)] sbyte Field02 { get; }
+        [Member(3)][MemberLayout(2)] byte Field03 { get; }
+        [Member(4)][MemberLayout(4)] short Field04 { get; }
+        [Member(5)][MemberLayout(6)] ushort Field05 { get; }
+        [Member(6)][MemberLayout(8)] char Field06 { get; }
+        [Member(7)][MemberLayout(12)] int Field07 { get; }
+        [Member(8)][MemberLayout(16)] uint Field08 { get; }
+        [Member(9)][MemberLayout(20)] float Field09 { get; }
 #if NET6_0_OR_GREATER
-        [Member(10)][MemberLayout(10, 2)] Half Field10 { get; }
+        [Member(10)][MemberLayout(10)] Half Field10 { get; }
 #endif
         int? OptionalField07 { get; }
 #if NET6_0_OR_GREATER
