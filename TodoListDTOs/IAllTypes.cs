e7064e4ec86c9d1b660c0b62e2c4d7007cf229c1
Psiman62 <simon_dudley@msn.com>
2024-10-20 13:37:02 +1000

    Core updates

--- TodoListDTOs/IAllTypes.cs
+++ TodoListDTOs/IAllTypes.cs
@@ -6,24 +6,24 @@ using System;
 namespace TodoListDTOs
 {
     [Entity]
-    [EntityLayout(LayoutMethod.Explicit, 128)]
+    [EntityLayout(LayoutMethod.Explicit, 256)]
     public interface IAllTypesExplicit
     {
-        [Member(1)][MemberLayout(0)] bool Field01 { get; }
-        [Member(2)][MemberLayout(1)] sbyte Field02 { get; }
-        [Member(3)][MemberLayout(2)] byte Field03 { get; }
-        [Member(4)][MemberLayout(4)] short Field04 { get; }
-        [Member(5)][MemberLayout(6)] ushort Field05 { get; }
-        [Member(6)][MemberLayout(8)] char Field06 { get; }
-        [Member(7)][MemberLayout(12)] int Field07 { get; }
-        [Member(8)][MemberLayout(16)] uint Field08 { get; }
-        [Member(9)][MemberLayout(20)] float Field09 { get; }
-        [Member(10)][MemberLayout(24)] long Field10 { get; }
-        [Member(11)][MemberLayout(32)] ulong Field11 { get; }
-        [Member(12)][MemberLayout(40)] double Field12 { get; }
-        [Member(13)][MemberLayout(48)] Guid Field13 { get; }
-        [Member(14)][MemberLayout(64)] Decimal Field14 { get; }
-        [Member(15)][MemberLayout(80)] DayOfWeek Field15 { get; }
+        [Member(1)][MemberLayout(0, 1)] bool Field01 { get; }
+        [Member(2)][MemberLayout(2, 3)] sbyte Field02 { get; }
+        [Member(3)][MemberLayout(4, 5)] byte Field03 { get; }
+        [Member(4)][MemberLayout(6, 8)] short Field04 { get; }
+        [Member(5)][MemberLayout(10, 12)] ushort Field05 { get; }
+        [Member(6)][MemberLayout(14, 16)] char Field06 { get; }
+        [Member(7)][MemberLayout(20, 24)] int Field07 { get; }
+        [Member(8)][MemberLayout(28, 32)] uint Field08 { get; }
+        [Member(9)][MemberLayout(36, 40)] float Field09 { get; }
+        [Member(10)][MemberLayout(48, 56)] long Field10 { get; }
+        [Member(11)][MemberLayout(64, 72)] ulong Field11 { get; }
+        [Member(12)][MemberLayout(80, 88)] double Field12 { get; }
+        [Member(13)][MemberLayout(96, 112)] Guid Field13 { get; }
+        [Member(14)][MemberLayout(128, 144)] Decimal Field14 { get; }
+        [Member(15)][MemberLayout(148, 152)] DayOfWeek Field15 { get; }
 
         //int? OptionalField07 { get; }
     }
