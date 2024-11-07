5821b8e02e50813888563979d4a784a544c60ce7
Psiman62 <simon_dudley@msn.com>
2024-11-07 11:20:08 +1000

    improved regression tests

--- TodoListDTOs.Tests/UnitTest1.cs
+++ TodoListDTOs.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using DTOMaker.Runtime;
 using FluentAssertions;
 using MessagePack;
+using System.Linq;
 
 namespace TodoListDTOs.Tests
 {
@@ -19,7 +20,48 @@ namespace TodoListDTOs.Tests
 
             ReadOnlyMemory<byte> buffer = orig.Block;
 
+            string.Join("-", buffer.ToArray().Select(b => b.ToString("X2"))).Should().Be(
+                "01-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-"+"" +
+                "7B-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-"+
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-"+
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-"+
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-"+
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-"+
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-"+
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00");
+
             var copy = new MemBlocks.AllTypesExplicit(buffer);
+            copy.Freeze();
+
+            copy.IsFrozen().Should().BeTrue();
+            copy.Field01.Should().Be(orig.Field01);
+            copy.Field08.Should().Be(orig.Field08);
+            //todo copy.Equals(orig).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Roundtrip_MemBlocks_Sequential()
+        {
+            var orig = new MemBlocks.AllTypesSequential
+            {
+                Field01 = true,
+                Field08 = 123
+            };
+            orig.Freeze();
+
+            ReadOnlyMemory<byte> buffer = orig.Block;
+
+            string.Join("-", buffer.ToArray().Select(b => b.ToString("X2"))).Should().Be(
+                "01-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-" + "" +
+                "7B-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-" +
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-" +
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-" +
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-" +
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-" +
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-" +
+                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00");
+            var copy = new MemBlocks.AllTypesExplicit(buffer);
+            copy.Freeze();
 
             copy.IsFrozen().Should().BeTrue();
             copy.Field01.Should().Be(orig.Field01);
@@ -38,6 +80,13 @@ namespace TodoListDTOs.Tests
             orig.Freeze();
 
             ReadOnlyMemory<byte> buffer = MessagePackSerializer.Serialize<TodoListDTOs.MessagePack.AllTypesExplicit>(orig);
+
+            string.Join("-", buffer.ToArray().Select(b => b.ToString("X2"))).Should().Be(
+                "DC-00-10-C0-C3-00-00-00-00-00-00-7B-CA-00-00-00-" +
+                "00-00-00-CB-00-00-00-00-00-00-00-00-D9-24-30-30-" +
+                "30-30-30-30-30-30-2D-30-30-30-30-2D-30-30-30-30-" +
+                "2D-30-30-30-30-2D-30-30-30-30-30-30-30-30-30-30-" +
+                "30-30-A1-30-00");
 
             var copy = MessagePackSerializer.Deserialize<TodoListDTOs.MessagePack.AllTypesExplicit>(buffer);
             copy.Freeze();
--- TodoListDTOs/IAllTypes.cs
+++ TodoListDTOs/IAllTypes.cs
@@ -6,24 +6,24 @@ using System;
 namespace TodoListDTOs
 {
     [Entity]
-    [EntityLayout(LayoutMethod.Explicit, 256)]
+    [EntityLayout(LayoutMethod.Explicit, 128)]
     public interface IAllTypesExplicit
     {
         [Member(1)][MemberOffset(0)] bool Field01 { get; }
-        [Member(2)][MemberOffset(2)] sbyte Field02 { get; }
-        [Member(3)][MemberOffset(4)] byte Field03 { get; }
-        [Member(4)][MemberOffset(6)] short Field04 { get; }
-        [Member(5)][MemberOffset(10)] ushort Field05 { get; }
-        [Member(6)][MemberOffset(14)] char Field06 { get; }
-        [Member(7)][MemberOffset(20)] int Field07 { get; }
-        [Member(8)][MemberOffset(28)] uint Field08 { get; }
-        [Member(9)][MemberOffset(36)] float Field09 { get; }
-        [Member(10)][MemberOffset(48)] long Field10 { get; }
-        [Member(11)][MemberOffset(64)] ulong Field11 { get; }
-        [Member(12)][MemberOffset(80)] double Field12 { get; }
-        [Member(13)][MemberOffset(96)] Guid Field13 { get; }
-        [Member(14)][MemberOffset(128)] Decimal Field14 { get; }
-        [Member(15)][MemberOffset(148)] int Field15_Data { get; }
+        [Member(2)][MemberOffset(1)] sbyte Field02 { get; }
+        [Member(3)][MemberOffset(2)] byte Field03 { get; }
+        [Member(4)][MemberOffset(4)] short Field04 { get; }
+        [Member(5)][MemberOffset(6)] ushort Field05 { get; }
+        [Member(6)][MemberOffset(8)] char Field06 { get; }
+        [Member(7)][MemberOffset(12)] int Field07 { get; }
+        [Member(8)][MemberOffset(16)] uint Field08 { get; }
+        [Member(9)][MemberOffset(20)] float Field09 { get; }
+        [Member(10)][MemberOffset(24)] long Field10 { get; }
+        [Member(11)][MemberOffset(32)] ulong Field11 { get; }
+        [Member(12)][MemberOffset(40)] double Field12 { get; }
+        [Member(13)][MemberOffset(48)] Guid Field13 { get; }
+        [Member(14)][MemberOffset(64)] Decimal Field14 { get; }
+        [Member(15)][MemberOffset(80)] int Field15_Data { get; }
 #if NET6_0_OR_GREATER
         DayOfWeek Field15 { get { return (DayOfWeek)Field15_Data; } }
 #endif
